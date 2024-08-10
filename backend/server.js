fdcf5d88f45b88adadc06d9e38959a6e25afa9ca
Aryan Srivastava <aryansri666@gmail.com>
2024-08-10 20:54:17 +0530

    third version

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,18 @@
 const express=require("express");
 const app=express();
-//const cors = require('cors');
 const cors = require('cors');
-app.use(cors({
-  origin: 'https://66b768d118f5a536dd87cc28--endearing-pixie-481872.netlify.app/', 
-}));
 const bodyParser = require('body-parser');
 const PORT=6600;
 const db=require("./db");
 const Data=require('./models/info');
 //app.use(cors());
 app.use(bodyParser.json());
-
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://66b7813c712ea1462dd76629--incandescent-chimera-dcde92.netlify.app'], // Adjust this based on your requirements
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
 app.get('/api/data', async (req, res) => {
     try {
       const data = await Data.find();
